3675ae5d83901978b4c4ea11ae26c1a6379a6b77
Antonio Ramirez <ajrmzcs@gmail.com>
2018-02-05 21:57:01 -0500

    First commit

---
+++ app/dependencies.php
@@ -0,0 +1,36 @@
+<?php
+// DIC configuration
+
+$container = $app->getContainer();
+
+// Twig views
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/resources/views', [
+        'cache' => false,
+    ]);
+
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+
+    return $view;
+};
+
+// Eloquent ORM
+$container['db'] = function ($container) {
+    $capsule = new \Illuminate\Database\Capsule\Manager;
+    $capsule->addConnection($container['settings']['db']);
+
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+
+    return $capsule;
+};
+
+// monolog
+$container['logger'] = function ($c) {
+    $settings = $c->get('settings')['logger'];
+    $logger = new Monolog\Logger($settings['name']);
+    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
+    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
+    return $logger;
+};
